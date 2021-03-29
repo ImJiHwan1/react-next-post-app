84acf30df73a9f7849a0f2c1be2873f494c31ac2
ImJiHwan1 <61612132+ImJiHwan1@users.noreply.github.com>
2021-03-29 19:07:14 +0900

    page

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from 'next'
+import React from 'react';
+
+import { getPosts, PostType } from '../api';
+import { MainLayout } from '../components/MainLayout';
+import { PostList } from '../components/PostList';
+
+interface Props {
+    posts: PostType[];
+}
+
+const Home: NextPage<Props> = ({ posts }) => (
+    <MainLayout title="홈">
+        <PostList posts={posts} />
+    </MainLayout>
+);
+
+Home.getInitialProps = async() => {
+    const { posts } = await getPosts();
+
+    return {
+        posts
+    };
+};
+
+export default Home;
